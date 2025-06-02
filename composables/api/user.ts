49c1f89b7f6d45fe14e6b45d1b828dfa5bd58ee9
p <you@example.com>
2025-06-02 11:40:18 +0800

    0

--- composables/api/user.ts
+++ composables/api/user.ts
@@ -8,12 +8,6 @@ export const registerAsync: any = async (data: IRegister) => {
   return response;
 };
 
-// 发送邮箱验证码
-export const sendCodeAsync: any = async (data: any) => {
-  const response = await useAPI.post(`/auth/register-code`, data);
-  return response;
-};
-
 // 重新发送邮件链接
 export const resendVerificationAsync: any = async (email: string) => {
   const response = await useAPI.get(`/auth/email/resend-verification/${email}`);
@@ -32,12 +26,6 @@ export const resetPasswordAsync: any = async (data: any) => {
   return response;
 };
 
-// 查询用户列表
-export const getAllUserListAsync: any = async (params: any) => {
-  const response = await useAPI.get(`/users/getAllUser`, params);
-  return response;
-};
-
 // 修改用户头像
 export const updateUserAvatarAsync: any = async (data: any) => {
   const response = await useAPI.put(`/users/updateAvatar`, data);
@@ -47,17 +35,5 @@ export const updateUserAvatarAsync: any = async (data: any) => {
 // 更新用户个人信息
 export const updatePersonInfoAsync: any = async (data: any) => {
   const response = await useAPI.put(`/users/updatePersonInfo`, data);
-  return response;
-};
-
-// QQ登录
-export const qqLoginASync: any = async (params: any) => {
-  const response = await useAPI.get(`https://graph.qq.com/oauth2.0/authorize`, params);
-  return response;
-};
-
-// 判断用户是否拥有管理权限
-export const getUserPermissionAsync: any = async () => {
-  const response = await useAPI.get(`/users/getUserPermission`);
   return response;
 };
--- composables/api/userResume.ts
+++ composables/api/userResume.ts
@@ -1,5 +1,5 @@
 // 更新在线简历设置
 export const updateOnlineResumeAsync: any = async (data: any) => {
   const response = await useAPI.put(`/userresume/updateOnlineResume`, data);
-  return response.data.value;
+  return response;
 };
